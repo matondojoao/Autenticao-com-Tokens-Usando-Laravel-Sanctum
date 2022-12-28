0a499b1d1187de12421aa10e3edb998b64e7006b
Matondo <matondojoaokitemoco@gmail.com>
2022-12-28 14:09:39 +0100

    Configurando base para API

---
+++ database/migrations/2022_12_28_130656_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('stock')->nullable();
+            $table->integer('minimum_stock')->nullable();
+            $table->integer('maximum_stock')->nullable();
+            $table->decimal('price_purchase');
+            $table->decimal('price_sale');
+            $table->string('tax')->nullable();
+            $table->decimal('discount')->nullable();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
