5e6ad7c9533b151afad4e3579e0d51b1ce10fb20
Matondo <matondojoaokitemoco@gmail.com>
2022-12-28 14:12:05 +0100

    Configurando base para API

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'name','description','stock','minimum_stock','maximum_stock',
+        'price_purchase','price_sale','tax','discount','active'
+    ];
 }
