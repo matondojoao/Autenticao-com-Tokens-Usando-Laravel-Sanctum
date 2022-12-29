316609f1a8022c02203901344d5ed03350e1dfab
Matondo <matondojoaokitemoco@gmail.com>
2022-12-29 08:39:15 +0100

    EndPoint de Registro de usuario

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -4,8 +4,27 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'=>'required|email|string',
+            'password'=>'required'
+        ]);
 
+        $credentials=$request->only('email','password');
+
+        if(!$Auth=Auth()->attempt($credentials)){
+           return response()->json([
+            'Error'=>'Falha ao tentar logar'
+           ],401);
+        }
+        $token=Auth()->user()->createAccessToken();
+        return response()->json([
+            'token'=>$token->plainTextToken
+           ],200);
+    }
 }
--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -4,8 +4,33 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(Request $request, User $user)
+    {
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email|string',
+            'password'=>'required'
+        ]);
+       try {
+          $userData=([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+          ]);
+        $user= User::create($userData);
+        return response()->json([
+            'Error'=>'Usuario Registrado com sucesso'
+        ],500);
+
+       } catch (\Throwable $th) {
+          return response()->json([
+            'Error'=>$th->getMessage()
+          ],500);
+       }
+    }
 }
